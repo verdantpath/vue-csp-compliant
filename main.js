83f6c6212c9bd9648437a26eb5f6cf500862ad02
verdantpath <m_dittmann@yahoo.com>
2019-09-01 14:43:51 -0700

    add html and js

---
+++ main.js
@@ -0,0 +1,16 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center the window on the screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height = 300;
+  chrome.app.window.create("index.html", {
+    id: "learningVueED",
+    outerBounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth - width) / 2),
+      top: Math.round((screenHeight - height) /2)
+    }
+  });
+});
